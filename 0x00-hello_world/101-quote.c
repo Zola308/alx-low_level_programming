31acbe00afbcc673d7a53522db62d4499d9724d6
Iwinosa <zwatchcollections@gmail.com>
2022-06-30 09:35:12 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
 /**
- *main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed be a new line, to standard error.
+ *main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", 
+ *               followed be a new line, to standard error.
  *
  * Return: Always 1.
  */
