39f1347d869ba4b7200c51e63ac50fb91ba967ba
Iwinosa <zwatchcollections@gmail.com>
2022-07-01 16:09:58 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,23 +3,16 @@
 #include <time.h>
 
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-  
 {
 int d;
-  
 for (d = '0'; d <= '9'; d++)
 putchar(d);
 putchar('\n');
-  
 return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,22 +3,16 @@
 #include <time.h>
 
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
 {
 char low;
-
 for (low = 'z'; low >= 'a'; low--)
 putchar(low);
 putchar('\n');
-  
 return (0);
 }
