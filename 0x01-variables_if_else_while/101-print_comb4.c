f7d6692011e5a49c84f4a751fb649311641483a5
Iwinosa <zwatchcollections@gmail.com>
2022-07-01 17:03:47 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,11 +6,11 @@
  * Return: Always (Success)
  */
 
-int main(void)  
+int main(void)
 {
 int c, i, k;
 for (c = '0'; c <= '9'; c++)
-}
+{
 for (i = '0'; i <= '9'; i++)
 {
 for (k = '0'; k <= '9'; k++)
@@ -26,7 +26,7 @@ putchar(',');
 putchar(' ');
 }
 }
-}
+}	  
 }
 }
 putchar('\n');
