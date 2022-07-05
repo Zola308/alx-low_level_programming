3f5a9df62283cacf760102fd59885a2efb3a03f6
Iwinosa <zwatchcollections@gmail.com>
2022-07-05 15:40:43 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- * print_alphabet - prints to string
- * Description: Prints alphabet in lowercase
+ *print_alphabet - prints to string
+ *
+ *Description: Prints alphabets in lowercase
  */
-
 #include "main.h"
 void print_alphabet(void)
+
 {
 char low;
-for (low = 'a'; low <= 'z'; ch++)
-{      
+for (low = 'a'; low <= 'z'; low++)
+{
 putchar(low);
-}  
+}
 putchar('\n');
 
 return;
