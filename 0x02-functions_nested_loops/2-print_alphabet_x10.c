b4d0d7b0b039414c0b71af35f0dc6eae54c33e61
Iwinosa <zwatchcollections@gmail.com>
2022-07-06 07:48:18 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,35 +1,21 @@
 #include "main.h"
 
 /**
-
  * print_alphabet_x10 - Prints the alphabets in lowercase ten times
-
  */
 
 void print_alphabet_x10(void)
-  
-{
-  
-  int i;
-  
-  int j;
-  
 
-  
-  for (i = 0; i < 10; i++)
-    
-    {
-      
-      for (j = 97; j <= 122; j++)
-	
-	{
-	  
-	  _putchar(j);
-	  
-	}
-      
-      _putchar('\n');
-      
-    }
-  
+{
+int i;
+int j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
 }
