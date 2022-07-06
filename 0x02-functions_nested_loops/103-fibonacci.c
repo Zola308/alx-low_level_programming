309987e02b8d826c8a849a2783a33717bd94da0a
Iwinosa <zwatchcollections@gmail.com>
2022-07-06 09:47:45 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints even fibonnaci numbers
+ *
+ * Return: Always 0 for success
+ */
+
+int main(void)
+
+{
+long int n;
+long int num1, num2, fn;
+num1 = 1;
+num2 = 2;
+fn = num1 + num2;
+n = 2;
+while (fn < 4000000)
+{
+if (fn % 2 == 0)
+{
+n = n + fn;
+}	  
+fn = num1 + num2;
+num1 = num2;
+num2 = fn;
+}
+printf("%ld\n", n);
+return (0);
+}
