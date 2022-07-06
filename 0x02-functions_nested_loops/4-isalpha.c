c1fa9ef9711a342c52ce310863734c693cdc8dcf
Iwinosa <zwatchcollections@gmail.com>
2022-07-06 08:10:33 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if c is an alphabet
+ * @c: is the character to be checked
+ *
+ * Return: 1 if c is an alphabet and 0 otherwise
+ */
+
+int _isalpha(int c)
+
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
