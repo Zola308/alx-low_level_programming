d59a8c420062448123b7a0e1f28d70d678542f26
Iwinosa <zwatchcollections@gmail.com>
2022-07-06 08:20:00 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @l: The character to be checked
+ *
+ * Return: returns the absolute value of l
+ */
+
+int _abs(int l)
+
+{
+if (l >= 0)
+{
+return (l);
+}
+else
+{
+return (l * -1);
+}
+}
