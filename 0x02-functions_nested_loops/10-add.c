7365a42972c643b29942879e06da2d183893c91f
Iwinosa <zwatchcollections@gmail.com>
2022-07-06 09:04:54 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - prints the sum of two integers
+ * @i: The character to be checked
+ * @j: The character to be checked
+ *
+ * Return: The value of the sum
+ */
+
+int add(int i, int j)
+{
+int sum;
+sum = (i + j);
+return (sum);
+}
