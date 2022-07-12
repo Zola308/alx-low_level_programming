f1f43efe395e12b5f5d7aea678f642055147a0ca
Iwinosa <zwatchcollections@gmail.com>
2022-07-12 10:01:19 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: The string to be reversed
+ *
+ * Return: no return
+ *
+ */
+
+void rev_string(char *s)
+
+{
+
+int i = 0;
+
+int a, b;
+
+char *rev, t;
+
+while (i >= 0)
+
+{
+
+if (s[i] == '\0')
+
+{
+
+break;
+
+}
+
+i++;
+
+}
+
+rev = s;
+
+for (a = 0; a < (i - 1); a++)
+
+{
+
+for (b = a + 1; b > 0; b--)
+
+{
+
+t = *(rev + b);
+
+*(rev + b) = *(rev + (b - 1));
+
+*(rev + (b - 1)) = t;
+
+}
+}
+}
+
