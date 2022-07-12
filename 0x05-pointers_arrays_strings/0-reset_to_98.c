c554b14ab35919b503b67a33364fc74e73fe18b9
Iwinosa <zwatchcollections@gmail.com>
2022-07-12 09:10:04 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *
+ *  * reset_to_98 - a function that takes an integer and update to 98.
+ *
+ *   *
+ *
+ *    * @n: input integer.
+ *
+ *     *
+ *
+ *      * Return: Always void.
+ *
+ *       *
+ *
+ *        * Description: change the integer to 98.
+ *
+ *         */
+
+void reset_to_98(int *n)
+
+{
+
+		*n = 98;
+}
