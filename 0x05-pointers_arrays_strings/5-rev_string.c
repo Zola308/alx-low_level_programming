64ece5fe0e4746e51d764a8e54512bac3b0f002a
Iwinosa <zwatchcollections@gmail.com>
2022-07-12 10:05:37 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,47 +11,26 @@
  */
 
 void rev_string(char *s)
-
 {
-
 int i = 0;
-
 int a, b;
-
 char *rev, t;
-
 while (i >= 0)
-
 {
-
 if (s[i] == '\0')
-
 {
-
 break;
-
 }
-
 i++;
-
 }
-
 rev = s;
-
 for (a = 0; a < (i - 1); a++)
-
 {
-
 for (b = a + 1; b > 0; b--)
-
 {
-
 t = *(rev + b);
-
 *(rev + b) = *(rev + (b - 1));
-
 *(rev + (b - 1)) = t;
-
 }
 }
 }
