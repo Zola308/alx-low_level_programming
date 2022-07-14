8f67bf2c4467c5647392b86c2fc3ca31f9bc7691
Iwinosa <zwatchcollections@gmail.com>
2022-07-14 12:58:47 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,64 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
+ * cap_string - Capitalizes all words of a string
  *
- * @str: the string to change the first letter of a word in uppercase
+ * @s: The string
  *
- * Return: capitalizes letters
+ * Return: the capitalizes string
  *
  */
 
-char *cap_string(char *str)
-
+char *cap_string(char *s)
 {
-
-int i;
-
-int j;
-
-char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
-
-
-
-i = 0;
-
-
-
-while (str[i] != '\0')
-
+int count = 0, i;
+int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
 {
-
-if (i == 0 && str[i] >= 97 && str[i] <= 122)
-
+for (i = 0; i < 13; i++)
 {
-
-str[i] = str[i] - 32;
-
+if (*(s + count) == sep_words[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
 }
-
-j = 0;
-
-while (c[j] != '\0')
-
-{
-
-if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-
-{
-
-str[i + 1] = str[i + 1] - 32;
-
 }
-
-j++;
-
+count++;
 }
-i++;
-
-}
-
-return (str);
-
+return (s);
 }
 
