09c11d201641f1f48d29df843346deda2bb67afd
Zola308 <107005258+Zola308@users.noreply.github.com>
2022-08-18 12:04:42 +0200

    10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a
+ * listint_t liked list.
+ * @head: the head of the list
+ * @index: the position of the deleted node
+ *
+ * Return: 1 for success and -1 for failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
+
+	prev = *head;
+	if (index != 0)
+	{
+		for (i = 0; i < index && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
+	next = prev->next;
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
+	return (1);
+}
